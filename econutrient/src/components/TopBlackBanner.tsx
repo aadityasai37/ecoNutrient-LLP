32d1080bf9c92bfb5f6d564054889145f2e0b759
N Aditya Sai <69337330+aadityasai37@users.noreply.github.com>
2025-09-25 14:40:30 +0530

    Delete econutrient directory

--- econutrient/src/components/TopBlackBanner.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-export default function TopBlackBanner() {
-  return (
-    <div className="w-full bg-black text-white text-xs md:text-sm py-2 text-center">
-      FREE SHIPPING ON PREPAID ORDERS
-    </div>
-  );
-}
